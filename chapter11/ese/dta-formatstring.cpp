191cdd3497aef5a0972f3ef0f1ae58149b9075f2
guestx86 <luca94dd@protonmail.com>
2020-03-26 12:29:29 +0100

    Initial commit

---
+++ chapter11/ese/dta-formatstring.cpp
@@ -0,0 +1,262 @@
+/*
+ * Simple DTA tool that detects format string vulnerabilities.
+ *
+ * See /usr/include/i386-linux-gnu/asm/unistd_32.h for x86 (32 bit) syscall numbers.
+ * See /usr/include/asm-generic/unistd.h for x64 syscall numbers.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <ctype.h>
+#include <assert.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#include <map>
+#include <string>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <linux/net.h>
+
+#include "pin.H"
+
+#include "branch_pred.h"
+#include "libdft_api.h"
+#include "syscall_desc.h"
+#include "tagmap.h"
+
+/* ins descriptors */
+extern ins_desc_t ins_desc[XED_ICLASS_LAST];
+
+/* syscall descriptors */
+extern syscall_desc_t syscall_desc[SYSCALL_MAX];
+
+static std::map<int, uint8_t> fd2color;
+static std::map<uint8_t, std::string> color2fname;
+
+#define MAX_COLOR 0x80
+#define DBG_PRINTS 1
+
+void alert(uintptr_t addr, uint8_t tag)
+{
+  fprintf(stderr, "\n(dta-formatstring) !!!!!!! ADDRESS 0x%x IS TAINTED (tag=0x%02x), ABORTING !!!!!!!\n",
+          addr, tag);
+
+  for (unsigned c = 0x01; c <= MAX_COLOR; c <<= 1)
+  {
+    if (tag & c)
+    {
+      fprintf(stderr, "  tainted by color = 0x%02x (%s)\n", c, color2fname[c].c_str());
+    }
+  }
+  exit(1);
+}
+
+/* ------- TAINT SOURCES ------- */
+
+uint8_t next_color = 0x01;
+
+//maps each opened file with a color.
+static void
+post_open_hook(syscall_ctx_t *ctx)
+{
+  uint8_t color;
+  int fd            =         (int)ctx->ret;
+  const char *fname = (const char*)ctx->arg[SYSCALL_ARG0];
+
+  if(unlikely((int)ctx->ret < 0)) {
+    return;
+  }
+
+  if(strstr(fname, ".so") || strstr(fname, ".so.")) {
+    return;
+  }
+
+#if DBG_PRINTS
+  fprintf(stderr, "(dta-formatstring) opening %s at fd %u with color 0x%02x\n", fname, fd, next_color);
+#endif
+
+  if(!fd2color[fd]) {
+    color = next_color;
+    fd2color[fd] = color;
+    if(next_color < MAX_COLOR) next_color <<= 1;
+  } else {
+    /* reuse color of file with same fd which was opened previously */
+    color = fd2color[fd];
+  }
+
+  /* multiple files may get the same color if the same fd is reused
+   * or we run out of colors */
+  if(color2fname[color].empty()) color2fname[color] = std::string(fname);
+  else color2fname[color] += " | " + std::string(fname);
+}
+
+//taints byte received from the network
+static void
+post_socketcall_hook(syscall_ctx_t *ctx)
+{
+  int fd;
+  void *buf;
+  size_t len;
+  uint8_t color;
+
+  int call            =            (int)ctx->arg[SYSCALL_ARG0];
+  unsigned long *args = (unsigned long*)ctx->arg[SYSCALL_ARG1];
+
+  switch(call) {
+  case SYS_RECV:
+  case SYS_RECVFROM:
+    if(unlikely(ctx->ret <= 0)) {
+      return;
+    }
+
+    fd  =    (int)args[0];
+    buf =  (void*)args[1];
+    len = (size_t)ctx->ret;
+
+    if(!fd2color[fd]) {
+      color = next_color;
+      fd2color[fd] = color;
+      if(next_color < MAX_COLOR) next_color <<= 1;
+    } else {
+      /* reuse color of file with same fd which was opened previously */
+      color = fd2color[fd];
+    }
+
+#if DBG_PRINTS
+    fprintf(stderr, "(dta-execve) recv: %zu bytes from fd %u\n", len, fd);
+
+    for(size_t i = 0; i < len; i++) {
+      if(isprint(((char*)buf)[i])) fprintf(stderr, "%c", ((char*)buf)[i]);
+      else                         fprintf(stderr, "\\x%02x", ((char*)buf)[i]);
+    }
+    fprintf(stderr, "\n");
+
+    fprintf(stderr, "(dta-execve) tainting bytes %p -- 0x%x with tag 0x%x\n", 
+            buf, (uintptr_t)buf+len, 0x01);
+#endif
+
+    tagmap_setn((uintptr_t)buf, len, color);
+
+    break;
+
+  default:
+    break;
+  }
+}
+
+static void
+post_read_hook(syscall_ctx_t *ctx)
+{
+  int fd     =    (int)ctx->arg[SYSCALL_ARG0];
+  void *buf  =  (void*)ctx->arg[SYSCALL_ARG1];
+  size_t len = (size_t)ctx->ret;
+  uint8_t color;
+
+  if(unlikely(len <= 0)) {
+    return;
+  }
+
+#if DBG_PRINTS
+  fprintf(stderr, "(dta-formatstring) read: %zu bytes from fd %u\n", len, fd);
+#endif
+
+  color = fd2color[fd];
+  if(color) {
+#if DBG_PRINTS
+    fprintf(stderr, "(dta-formatstring) tainting bytes %p -- 0x%x with color 0x%x\n", 
+            buf, (uintptr_t)buf+len, color);
+#endif
+    tagmap_setn((uintptr_t)buf, len, color);
+  } else {
+#if DBG_PRINTS
+    fprintf(stderr, "(dta-formatstring) clearing taint on bytes %p -- 0x%x\n",
+            buf, (uintptr_t)buf+len);
+#endif
+    tagmap_clrn((uintptr_t)buf, len);
+  }
+}
+
+/* ------- TAINT SINKS ------- */
+
+//check if the format string of the called function is tainted.
+void check_tainted_string_fmt(ADDRINT arg)
+{
+  char * format_string = (char *)arg;
+  uint8_t tag;
+
+  for(char * it = format_string; *it != '\0'; it++) {
+    tag = tagmap_getb((uintptr_t)it);
+    if(tag != 0) alert((uintptr_t)it, tag);
+  }
+}
+
+static void
+dta_instrument_call(INS ins)
+{
+  // if (likely(INS_IsDirectBranchOrCall(ins))) <-- for performance. TODO check the usage of likely keyword
+  if (INS_IsDirectBranchOrCall(ins))
+  {
+    int arg_num = 0;
+    std::string func = RTN_FindNameByAddress(INS_DirectBranchOrCallTargetAddress(ins));
+
+    if (func.find("printf") != string::npos)
+    {
+      if(func.compare("printf") == 0 || func.compare("vprintf") == 0)
+      {
+        arg_num = 0;
+      } 
+      else if(func.compare("fprintf") == 0 || func.compare("dprintf") == 0 ||
+              func.compare("sprintf") == 0 || func.compare("vfprintf") == 0 ||
+              func.compare("vdprintf") == 0 || func.compare("vsprintf") == 0)
+      {
+        arg_num = 1;
+      }
+      else if(func.compare("snprintf") == 0 || func.compare("vsnprintf") == 0)
+      {
+        arg_num = 2;
+      }
+
+      INS_InsertCall(ins,
+        IPOINT_BEFORE,
+        (AFUNPTR)check_tainted_string_fmt,
+        IARG_FUNCARG_CALLSITE_VALUE, arg_num,
+        IARG_END);
+    }
+  }
+}
+
+/* --------- MAIN ---------------*/
+
+int main(int argc, char **argv)
+{
+  PIN_InitSymbols();
+
+  if (unlikely(PIN_Init(argc, argv)))
+  {
+    return 1;
+  }
+
+  if (unlikely(libdft_init() != 0))
+  {
+    libdft_die();
+    return 1;
+  }
+
+  syscall_set_post(&syscall_desc[__NR_open], post_open_hook);
+  syscall_set_post(&syscall_desc[__NR_read], post_read_hook);
+  syscall_set_post(&syscall_desc[__NR_socketcall], post_socketcall_hook);
+
+  /* instrument call */
+  (void)ins_set_pre(&ins_desc[XED_ICLASS_CALL_NEAR],
+                    dta_instrument_call);
+
+  PIN_StartProgram();
+
+  return 0;
+}
---
+++ chapter11/ese/dta-formatstring_test.c
@@ -0,0 +1,109 @@
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <signal.h>
+
+void vuln_func(char *buf)
+{
+  unsigned int i;
+  char *ptr;
+
+  if (!(ptr = malloc(8)))
+    return;
+
+  for (i = 0; i < 8; i++)
+    ptr[i] = buf[i];
+
+  printf(ptr);
+  free(ptr);
+}
+
+int
+open_socket(const char *node, const char *service)
+{
+  struct addrinfo hints, *res;
+  int sockfd;
+
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family   = AF_INET;
+  hints.ai_socktype = SOCK_DGRAM;
+  hints.ai_flags    = AI_PASSIVE;
+  if(getaddrinfo(NULL, "9999", &hints, &res) != 0) {
+    return -1;
+  }
+
+  if((sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) < 0) {
+    return -1;
+  }
+  if(bind(sockfd, res->ai_addr, res->ai_addrlen) < 0) {
+    return -1;
+  }
+
+  return sockfd;
+}
+
+int main(int ac, char **av)
+{
+  int  fd1, fd2, fd3, fd4;
+  char *buf1, *buf2, *buf3, *buf4;
+  
+  char socket_buf[8];
+  socklen_t addrlen;
+  struct sockaddr_storage addr;
+
+  if (!(buf1 = malloc(8)))
+    return -1;
+  if (!(buf2 = malloc(8)))
+    return -1;
+  if (!(buf3 = malloc(8)))
+    return -1;
+  if (!(buf4 = malloc(8)))
+    return -1;
+  
+  fd1 = open("/home/binary/code/chapter11/ese/1.txt", O_RDONLY);
+  read(fd1, buf1, 8);
+
+  fd2 = open("/home/binary/code/chapter11/ese/2.txt", O_RDONLY);
+  read(fd2, buf2, 8);
+
+  fd3 = open("/home/binary/code/chapter11/ese/3.txt", O_RDONLY);
+  read(fd3, buf3, 8);
+
+  fd4 = open("/home/binary/code/chapter11/ese/4.txt", O_RDONLY);
+  read(fd4, buf4, 8);
+
+  int sockfd = open_socket("localhost", "9999");
+  
+  if(sockfd < 0) {
+    fprintf(stderr, "failed to open socket\n");
+    return 1;
+  }
+
+  addrlen = sizeof(addr);
+  if(recvfrom(sockfd, socket_buf, sizeof(socket_buf), 0, (struct sockaddr*)&addr, &addrlen) < 0) {
+    fprintf(stderr, "(dta-formatstring_test) recvfrom failed\n");
+    return 1;
+  }
+
+  //printf("%d\n%d\n%d\n%d\n%d\n", fd1,fd2,fd3,fd4,sockfd);
+  
+  close(fd1);
+  close(fd2);
+  close(fd3);
+  close(fd4);
+  close(sockfd);
+
+  //vuln_func(buf3);
+  vuln_func(socket_buf);
+
+  free(buf1);
+  free(buf2);
+  free(buf3);
+  free(buf4);
+}
